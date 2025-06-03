0d2036a47c7b64cf7017b58312907aa444ef3b07
EchoGinDev <leonsackerman@gmail.com>
2025-06-03 11:06:58 +0700

    commit 1

--- admin_menu.php
+++ admin_menu.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
+    <link rel="stylesheet" href="styles.css"> <!-- CSS eksternal -->
 </head>
 <body>
+    <!-- Bagian Header / Navbar -->
+<header>
+    <nav class="navbar">
+        <a href="admin_menu.php">
+            <img class="logo" src="images/navbarLogo.png" alt="Logo Perusahaan">
+        </a>
+        <ul class="nav-links">
+            <li><a href="#">About</a></li>
+            <li><a href="admin_menu.php">Home</a></li>
+            <li><a href="login.php" class="contact-btn">Login</a></li>
+        </ul>
+    </nav>
+</header>
     
 </body>
 </html>
